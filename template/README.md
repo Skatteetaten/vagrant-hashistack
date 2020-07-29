1b2c5a1ce071d851599cc3c142bf3b5825503eb3
Per Daniel Thorsrud <perdaniel.thorsrud@skatteetaten.no>
2020-07-29 15:15:28 +0200

    Added documentation to using ACLs in nomad together with vault

--- template/README.md
+++ template/README.md
@@ -8,7 +8,7 @@ If you found this in `fredrikhgrelland/vagrant-hashistack`, you may be intereste
 
 Documentation on [parent repository](https://github.com/fredrikhgrelland/vagrant-hashistack#usage).
 
-## Customizing the vagrant box
+## Customizing and using the vagrant box
 The vagrant box ships with a default startup scheme. It will run an ansible playbook to start all services.
 You may change the hashistack configuration or add aditional pre and post steps to the startup procedure to match your needs.
 
--- template/terraform/main.tf
+++ template/terraform/main.tf
@@ -1,6 +1,22 @@
+provider "vault" {
+  address = "http://127.0.0.1:8200"
+  token = "master"
+}
+
 provider "nomad" {
   address = "http://127.0.0.1:4646"
+  // The following line is needed if ACLs are enabled in Nomad
+  //  secret_id = "${data.vault_generic_secret.nomad_secret_id.data.secret_id}"
 }
+
+// The following data-source is needed ACLs are enabled in Nomad
+/*
+data "vault_generic_secret" "nomad_secret_id" {
+  path = "nomad/creds/write"
+}
+*/
+
+
 /*
 resource "nomad_job" "your_nomad_job" {
   jobspec = file("${path.cwd}/../nomad/your_nomad_job.hcl")
