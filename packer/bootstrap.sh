1eacc8f274b10b7219497adb84537c13b81ed50c
Fredrik Hoem Grelland <fredrikhoem.grelland@skatteetaten.no>
2020-08-07 12:26:21 +0200

    Suppress warning

--- packer/bootstrap.sh
+++ packer/bootstrap.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 #Grub update forces interactive that is why this seemingly crazy
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -q -y -u -o Dpkg::Options::="--force-confdef" --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-change-held-packages --allow-unauthenticated;
-sudo apt-get install -y python3-distutils jq unzip && curl -k -s https://bootstrap.pypa.io/get-pip.py | sudo python3
-sudo pip install ansible
+sudo apt-get install -y python3-distutils jq unzip && curl -k -s https://bootstrap.pypa.io/get-pip.py | sudo -H python3
+sudo -H pip install ansible
