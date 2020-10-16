c6868b65393072093aef5b51a74795de9a6c381c
k16592 <danielgerhardsen.nilsen@skatteetaten.no>
2020-10-16 08:12:21 +0200

    fix template

--- template/template_example/dev/vagrant/bootstrap/pre_ansible.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Rewiring copied paths ´/etc/ansible´ onto their mounted git-source ´/box/ansible´"
-sudo mv /etc/ansible /etc/ansible.packaged
-sudo ln -s /box/ansible /etc/ansible
