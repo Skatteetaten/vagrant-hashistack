bbffae1af229c0a747f9bfe7e85223d20922a550
dangernil <71001093+dangernil@users.noreply.github.com>
2020-11-23 08:41:22 +0100

    test with random nomad token (#448)
    * Running tests with random token

--- .github/action/create-env.py
+++ .github/action/create-env.py
@@ -1,16 +1,18 @@
 import os
-
-env_keys = list(dict(os.environ).keys())
+from uuid import uuid4
 
 out_file = ""
 filepath = os.path.join(
     str(os.environ.get("GITHUB_WORKSPACE")), str(os.environ.get("FILE_NAME"))
 )
 
-for key in env_keys:
+for key in os.environ.keys():
     if key.startswith("ENVKEY_"):
-        out_file += key.split("ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
-filepath
+        if key.endswith("master_token") and os.environ.get(key) == 'random':
+            out_file += key.split("ENVKEY_")[1] + "=" + uuid4().hex + "\n"
+        else:
+            out_file += key.split("ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
+
 with open(filepath, "w") as text_file:
     text_file.write(out_file)
 
